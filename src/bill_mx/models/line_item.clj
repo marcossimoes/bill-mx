46bf9caa1026323c043f1f1c068eaa14604d9a64
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-19 13:38:06 -0300

    add dates dependencies in line-item model

--- src/bill_mx/models/line_item.clj
+++ src/bill_mx/models/line_item.clj
@@ -1,6 +1,7 @@
 (ns bill-mx.models.line-item
   (:require [clojure.spec.alpha :as s]
-            [bill-mx.models.general :as g]))
+            [bill-mx.models.general :as g]
+            [clj-time.core :as t]))
 
 ;; LINE ITEMS TYPES
 (s/def ::id ::g/id)
@@ -10,4 +11,5 @@
 (s/def ::due-date ::g/date-type)
 
 ;; LINE ITEM
-(s/def ::line-item (s/keys :req [::id ::type ::amount ::clear-date ::due-date]))
+(s/def ::line-item (s/and (s/keys :req [::id ::type ::amount ::clear-date ::due-date])
+                          #(t/after? (::due-date %) (::clear-date %))))
