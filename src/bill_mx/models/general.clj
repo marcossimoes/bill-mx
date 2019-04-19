228a26486717ea2d2721e1b706b60efec4cef113
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-19 13:37:11 -0300

    change ::date-time to clj-time local-date-time

--- src/bill_mx/models/general.clj
+++ src/bill_mx/models/general.clj
@@ -1,10 +1,11 @@
 (ns bill-mx.models.general
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clj-time.spec :as ts]))
 
 ;; GENERAL TYPES
 (s/def ::money-type decimal?)
 (s/def ::rate decimal?)
-(s/def ::date-type inst?)
+(s/def ::date-type ::ts/local-date-time)
 (s/def ::num-days int?)
 (s/def ::day-of-month int?)
 (s/def ::id uuid?)
