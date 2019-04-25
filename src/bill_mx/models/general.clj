b74eab13383cd3494a6d993bc4e1200a65ad9eef
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-25 20:57:38 -0300

    improve num-days-type and day-of-month type

--- src/bill_mx/models/general.clj
+++ src/bill_mx/models/general.clj
@@ -1,12 +1,13 @@
 (ns bill-mx.models.general
   (:require [clojure.spec.alpha :as s]
-            [clj-time.spec :as ts]))
+            [clj-time.spec :as ts]
+            [bill-mx.numbers :as n]))
 
 ;; GENERAL TYPES
 (s/def ::money-type decimal?)
 (s/def ::rate decimal?)
 (s/def ::date-type ::ts/local-date-time)
-(s/def ::num-days int?)
-(s/def ::day-of-month int?)
+(s/def ::num-days (s/and int? n/not-neg?))
+(s/def ::day-of-month (set (range 1 (inc 31))))
 (s/def ::id uuid?)
 (s/def ::type string?)
