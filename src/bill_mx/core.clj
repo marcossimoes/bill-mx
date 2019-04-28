f45b3c3d9050b48bf1d763ab1720be807a55b7cb
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-28 20:42:40 -0300

    add open date and due date calc capability

--- src/bill_mx/core.clj
+++ src/bill_mx/core.clj
@@ -1,7 +1,41 @@
 (ns bill-mx.core
+  (:require [bill-mx.time :as bt]
+            [clojure.spec.alpha :as s]
+            [bill-mx.models.bill :as bm]
+            [bill-mx.models.cc_acc :as cca])
   (:gen-class))
 
+(defn open-date
+  "Calculates open date from previous bill close date"
+  [bill prev-bill-close-date]
+  (assoc bill :open-date prev-bill-close-date))
+
+(s/fdef open-date
+        :args (s/cat :bill ::bm/bill
+                     :prev-bill-close-date ::bm/close-date)
+        :ret ::bm/bill)
+
+(defn due-date
+  "Receives a bill with open-date and a contracted-due-day-of-month
+  and Returns a bill with due date"
+  [bill due-day]
+  (let [open-date (:open-date bill)
+        due-date (bt/nxt-n-day open-date due-day)]
+    (assoc bill :due-date due-date)))
+
+(s/fdef due-date
+        :args (s/cat :bill ::bm/bill
+                     :due-day ::cca/contracted-due-day-of-month)
+        :ret ::bm/bill)
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [prev-bill cc-acc]
+  (-> (s/conform ::bm/bill {::bm/status :draft})
+      (open-date (:close-date prev-bill))
+      (due-date (:contracted-due-day-of-month cc-acc))
+      ;(effective-due-date)
+      ;(close-date (:grace-period cc-acc))
+      ;(status prev-bill cc-acc)
+      ))
+
+;;TODO: create bill schema?
