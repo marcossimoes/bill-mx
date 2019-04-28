69d4772f67a2d1361ead48a2c23c6f6b5e8dd533
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-28 20:42:07 -0300

    add clj coerce type in general types

--- src/bill_mx/models/general.clj
+++ src/bill_mx/models/general.clj
@@ -6,8 +6,11 @@
 ;; GENERAL TYPES
 (s/def ::money-type decimal?)
 (s/def ::rate decimal?)
-(s/def ::date-type ::ts/local-date-time)
+(s/def ::date-type ::ts/date-time)
 (s/def ::num-days (s/and int? n/not-neg?))
 (s/def ::day-of-month (set (range 1 (inc 31))))
 (s/def ::id uuid?)
 (s/def ::type string?)
+(s/def ::clj-time-coerce-type (s/or :inst inst?
+                                    :int integer?
+                                    :str string?))
