ccddca97384982e2353ad0ba5ccc3e7345b8e003
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-28 20:12:30 -0300

    inlcude nil as a possible status for bill

--- src/bill_mx/models/bill.clj
+++ src/bill_mx/models/bill.clj
@@ -7,7 +7,7 @@
 
 ;; BILL ITEMS TYPES
 
-(s/def ::status #{:future :open :closed :late :paid})
+(s/def ::status #{:future :open :closed :late :paid nil})
 (s/def ::effective-due-date ::g/date-type)
 (s/def ::due-date ::g/date-type)
 (s/def ::open-date ::g/date-type)
@@ -20,29 +20,52 @@
 
 ;; BILL
 
+(def not-neg-amount-paid #(n/not-neg? (::amount-paid %)))
+(def not-neg-min-pmt #(n/not-neg? (::min-pmt %)))
+(def effective=>duedate=>closedate #(bt/date-equal-or-after? (::effective-due-date %)
+                                           (::due-date %)
+                                           (::close-date %)))
+(def close-date>open-date #(bt/date-after? (::close-date %) (::open-date %)))
+
 (defmulti bill-status ::status)
 
 (defmethod bill-status :future [_]
-  (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total]))
+  (s/and (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total])
+         effective=>duedate=>closedate
+         ))
 
 (defmethod bill-status :open [_]
   (s/and (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total ::amount-paid])
-         #(n/not-neg? (::amount-paid %))))
+         effective=>duedate=>closedate
+         close-date>open-date
+         not-neg-amount-paid
+         ))
 
 (defmethod bill-status :closed [_]
   (s/and (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total ::amount-paid ::min-pmt])
-         #(n/not-neg? (::amount-paid %) (::min-pmt %))))
+         effective=>duedate=>closedate
+         close-date>open-date
+         not-neg-amount-paid
+         not-neg-min-pmt
+         ))
 
 (defmethod bill-status :paid [_]
   (s/and (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total ::amount-paid])
-         #(n/not-neg? (::amount-paid %))))
+         effective=>duedate=>closedate
+         close-date>open-date
+         not-neg-amount-paid
+         ))
 
 (defmethod bill-status :late [_]
   (s/and (s/keys :req [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total ::amount-paid ::min-pmt])
-         #(n/not-neg? (::amount-paid %) (::min-pmt %))))
+         effective=>duedate=>closedate
+         close-date>open-date
+         not-neg-amount-paid
+         not-neg-min-pmt
+         ))
 
-(s/def ::bill (s/and (s/multi-spec bill-status ::status)
-                     #(bt/date-equal-or-after? (::effective-due-date %)
-                                            (::due-date %)
-                                            (::close-date %))
-                     #(bt/date-after? (::close-date %) (::open-date %))))
+(defmethod bill-status nil [_]
+  (s/keys :req []
+          :opt [::status ::effective-due-date ::due-date ::open-date ::close-date ::current-date ::line-items ::total ::amount-paid ::min-pmt]))
+
+(s/def ::bill (s/and (s/multi-spec bill-status ::status)))
