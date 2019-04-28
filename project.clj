0f16a5bc7edee3d912eee70ea68394d66213b37f
Marcos Simões <marcos.simoes@nubank.com.br>
2019-04-28 16:29:28 -0300

    update project with clj/test and clj-time

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-time "0.15.0"]
-                 [lein-midje "3.2.1"]]
+                 [clj-time "0.15.0"]]
   :main ^:skip-aot bill-mx.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
