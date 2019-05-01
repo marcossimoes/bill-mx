69a17b28c38e0dd23a43840ce336c4eb16b7acac
Marcos Simões <marcos.simoes@nubank.com.br>
2019-05-01 18:29:47 -0300

    change date-types in time func

--- src/bill_mx/time.clj
+++ src/bill_mx/time.clj
@@ -13,10 +13,10 @@
   "Checks if the dates provided are the same
   or in order from the first being after the second and so on"
   ([date-a date-b]
-   (bm/when-let* [formated-date-a (co/to-local-date date-a)
-                  formated-date-b (co/to-local-date date-b)]
-     (not (t/before? formated-date-a
-                     formated-date-b))))
+   (bm/when-let* [formatted-date-a (co/to-date-time date-a)
+                  formatted-date-b (co/to-date-time date-b)]
+     (not (t/before? formatted-date-a
+                     formatted-date-b))))
   ([date-a date-b & other-dates]
    (->> (concat [date-a date-b] other-dates)
         ;; first brakes the list of local-dates in pairs to be compared
@@ -38,10 +38,10 @@
   "Checks if the date on a local-date-time-a
   is after a local-date-time-b"
   [date-a date-b]
-  (bm/when-let* [formated-date-a (co/to-local-date date-a)
-                 formated-date-b (co/to-local-date date-b)]
-                (t/after? formated-date-a
-                          formated-date-b)))
+  (bm/when-let* [formatted-date-a (co/to-date-time date-a)
+                 formatted-date-b (co/to-date-time date-b)]
+                (t/after? formatted-date-a
+                          formatted-date-b)))
 
 (s/fdef date-after?
         :args (s/cat :date-a ::g/clj-time-coerce-type
@@ -52,14 +52,17 @@
   "Receives a bill with open-date and a contracted-due-day-of-month
   and Returns a bill with due date"
   [date day-of-month]
-  (let [days-to (- day-of-month (t/day date))]
-    (t/plus date (t/days days-to) (if (n/not-pos? days-to) (t/months 1)))))
+  (bm/when-let* [formatted-date (co/to-date-time date)
+                 days-to (- day-of-month (t/day formatted-date))]
+    (t/plus formatted-date (t/days days-to) (if (n/not-pos? days-to) (t/months 1)))))
 
 (s/fdef nxt-n-day
-        :args (s/cat :date ::g/date-type
+        :args (s/cat :date ::g/clj-time-coerce-type
                      :day-of-month ::g/day-of-month)
-        :ret ::g/date-type
-        :fn #(t/after? (:ret %) (-> % :args :date)))
+        :ret ::g/date
+        :fn #(t/after? (:ret %) (co/to-local-date (-> % :args :date (second)))))
+
+;FIXME: stest/check not working for nxt-n-day
 
 (def holidays
   (set (map (partial apply t/date-time) [[2019 1 1] [2019 1 6] [2019 2 2] [2019 2 4]
@@ -78,11 +81,12 @@
 (defn contains-date?
   "Takes a date and returns true if the date is in a specific vector"
   [holidays date]
-  (some (partial pr/same-date? date) holidays))
+  (let [formatted-date (co/to-date-time date)]
+    (some (partial pr/same-date? formatted-date) holidays)))
 
 (s/fdef contains-date?
-        :args (s/cat :holidays (s/coll-of ::g/date-type)
-                     :date ::g/date-type)
+        :args (s/cat :holidays (s/coll-of ::g/date)
+                     :date ::g/clj-time-coerce-type)
         :ret (s/or :boolean boolean? :nil nil?))
 
 ;; TODO: make contains-date accept date in any format compatible with clj-time coerce
@@ -91,33 +95,42 @@
   "provided a date and a list of holidays returns true if the date is both a weekday
   and not a holiday"
   [date holidays]
-  (and (pr/weekday? date) (not (contains-date? holidays date))))
+  (let [formatted-date (co/to-date-time date)]
+    (and (pr/weekday? formatted-date) (not (contains-date? holidays formatted-date)))))
 
 (s/fdef biz-day?
-        :args (s/cat :date ::g/date-type
-                     :holidays (s/coll-of ::g/date-type))
+        :args (s/cat :date ::g/clj-time-coerce-type
+                     :holidays (s/coll-of ::g/date))
         :ret boolean?)
 
 (defn nxt-day
   "provided a date returns the nxt day"
   [date]
-  (t/plus date (t/days 1)))
+  (let [formatted-date (co/to-date-time date)]
+    (t/plus formatted-date (t/days 1))))
 
 (s/fdef nxt-day
-        :args ::g/date-type
-        :ret ::g/date-type
-        :fn #(t/after? (:ret %) (-> % :args :date)))
+        :args ::g/clj-time-coerce-type
+        :ret ::g/date
+        :fn #(t/after? (:ret %) (co/to-local-date (-> % :args :date (second)))))
+
+;FIXME: stest/check not working for nxt-day
 
 (defn nxt-biz-day-incl
   "Provided a date and a list of holidays return the nxt week day that is not
   a holiday. if the provided date is a week day and not a holiday returns the same date"
   [date holidays]
-  (if (biz-day? date holidays) date (nxt-biz-day-incl (nxt-day date) holidays)))
+  (let [formatted-date (co/to-date-time date)]
+    (if (biz-day? formatted-date holidays)
+      formatted-date
+      (nxt-biz-day-incl (nxt-day formatted-date) holidays))))
 
 (s/fdef nxt-biz-day-incl
-        :args (s/cat :date ::g/date-type
-                     :holidays (s/coll-of ::g/date-type))
-        :ret ::g/date-type
-        :fn #(t/after? (:ret %) (-> % :args :date)))
+        :args (s/cat :date ::g/clj-time-coerce-type
+                     :holidays (s/coll-of ::g/date))
+        :ret ::g/date
+        :fn #(t/after? (:ret %) (co/to-local-date (-> % :args :date (second)))))
+
+;FIXME: stest/check not working for nxt-biz-day-incl
 
 ;;TODO: would it make more sense to model holidays as a file instead of a hardcoded list here?
