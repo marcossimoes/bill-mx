f35006509034094de7f892adfa374871ef1e0123
Marcos Simões <marcos.simoes@nubank.com.br>
2019-05-01 19:09:26 -0300

    add effective due date capability

--- src/bill_mx/core.clj
+++ src/bill_mx/core.clj
@@ -1,9 +1,13 @@
 (ns bill-mx.core
-  (:require [bill-mx.time :as bt]
-            [clojure.spec.alpha :as s]
+  (:require [clojure.spec.alpha :as s]
+            [clj-time.core :as t]
+            [clj-time.predicates :as pr]
+            [bill-mx.models.general :as g]
             [bill-mx.models.bill :as bm]
-            [bill-mx.models.cc_acc :as cca])
-  (:gen-class))
+            [bill-mx.models.cc-acc :as cca]
+            [bill-mx.time :as bt])
+  ;(:gen-class)
+  )
 
 (defn open-date
   "Calculates open date from previous bill close date"
@@ -13,7 +17,7 @@
 (s/fdef open-date
         :args (s/cat :bill ::bm/bill
                      :prev-bill-close-date ::bm/close-date)
-        :ret ::bm/bill)
+        :ret (s/and ::bm/bill #(s/valid? ::bm/open-date (-> % :open-date))))
 
 (defn due-date
   "Receives a bill with open-date and a contracted-due-day-of-month
@@ -24,16 +28,33 @@
     (assoc bill :due-date due-date)))
 
 (s/fdef due-date
-        :args (s/cat :bill ::bm/bill
+        :args (s/cat :bill (s/and ::bm/bill #(contains? % :open-date))
                      :due-day ::cca/contracted-due-day-of-month)
-        :ret ::bm/bill)
+        :ret (s/and ::bm/bill
+                    #(s/valid? ::bm/due-date (-> % :due-date))
+                    #(s/valid? ::bm/open-date (-> % :open-date))))
+
+;;FIXME: stest/check not working for open date and due date due to bill schema
+
+(defn effective-due-date
+  "Receives a bill with a due date and a list of holdiays dates and return the
+  nxt business date including the actual due date as a possible business date"
+  [bill holidays]
+  (assoc bill :effective-due-date (bt/nxt-biz-day-incl (:due-date bill) holidays)))
+
+(s/fdef effective-due-date
+        :args (s/cat :bill (s/and ::bm/bill #(contains? % :due-date))
+                     :holidays (s/coll-of ::g/date))
+        :ret (s/and ::bm/bill
+                    #(s/valid? ::bm/due-date (-> % :due-date))
+                    #(s/valid? ::bm/effective-due-date (-> % :effective-due-date))))
 
 (defn -main
   [prev-bill cc-acc]
   (-> (s/conform ::bm/bill {::bm/status :draft})
       (open-date (:close-date prev-bill))
       (due-date (:contracted-due-day-of-month cc-acc))
-      ;(effective-due-date)
+      (effective-due-date bt/holidays)
       ;(close-date (:grace-period cc-acc))
       ;(status prev-bill cc-acc)
       ))
